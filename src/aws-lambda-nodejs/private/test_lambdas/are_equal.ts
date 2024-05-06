812792950217d5d3578613847954a3579d2ea13a
Michael Antonio <michanto@amazon.com>
2024-05-06 10:10:57 -0700

    Run InlineNodejsFunction integration test.

--- src/aws-lambda-nodejs/private/test_lambdas/are_equal.ts
+++ src/aws-lambda-nodejs/private/test_lambdas/are_equal.ts
@@ -1,6 +1,6 @@
 // Simple task for integration tests.
 export function areEqual(leftHandSide: string, rightHandSide: string) {
-    if(leftHandSide != rightHandSide) {
-        throw new Error(`${leftHandSide} != ${rightHandSide}`)
-    }
+  if (leftHandSide != rightHandSide) {
+    throw new Error(`${leftHandSide} != ${rightHandSide}`);
+  }
 }
--- test/aws-lambda-nodejs/integ.inline_node_js_function.ts
+++ test/aws-lambda-nodejs/integ.inline_node_js_function.ts
@@ -1,18 +1,25 @@
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
-import { App, RemovalPolicy, Stack } from 'aws-cdk-lib';
-import { Topic } from 'aws-cdk-lib/aws-sns';
+import { App, Stack } from 'aws-cdk-lib';
 import { InlineNodejsFunction } from '../../src/aws-lambda-nodejs';
 
 const app = new App();
 const stack = new Stack(app, 'IntegrationTestExampleStack', {});
-const LAMBDA_PATH = `${__dirname}/../lib/aws-lambda-nodejs/test_lambdas/`
-new InlineNodejsFunction(stack, "Reverse", {
-    entry: `${LAMBDA_PATH}/reverse_greeting.js`,
-    handler: 'reverseGreeting'
-})
+const LAMBDA_PATH = `${__dirname}/../../lib/aws-lambda-nodejs/private/test_lambdas/`;
+new InlineNodejsFunction(stack, 'Reverse', {
+  entry: `${LAMBDA_PATH}reverse_greeting.js`,
+  handler: 'reverseGreeting',
+});
 
 new IntegTest(app, 'IntegTestExample', {
   testCases: [
     stack,
   ],
+  cdkCommandOptions: {
+    destroy: {
+      args: {
+        force: true,
+      },
+    },
+  },
+  regions: ['us-east-1'],
 });
