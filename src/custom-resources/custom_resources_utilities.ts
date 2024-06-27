0eaad6c84482f1a2086ede1aca288511bbef9d95
Michael Antonio <michanto@amazon.com>
2024-06-27 12:08:05 -0700

    feat:  Add predicates to utilities classes. (#23)
    Make it easier for users to do their own searches on top of what we already provide.
    Also add StackUtilities.

--- src/custom-resources/custom_resources_utilities.ts
+++ src/custom-resources/custom_resources_utilities.ts
@@ -1,43 +1,49 @@
 import { CfnElement, CfnResource, Stack } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { ConstructTreeSearch } from '../core';
+import { CfnResourcePredicate, ConstructTreeSearch } from '../core';
 
-/**
- * Utilities for creating custom resources
- */
+/** Find CustomResource L1s (CfnResources) in the construct tree. */
 export class CustomResourceUtilities {
   /**
    * Checks if `elt` if a L1 CustomResource construct (CfnResource).
+   * Test is that elt is a CfnResource with a resourceType of the form
+   * 'Custom::XXX' or 'AWS::CloudFormation::CustomResource'.
    *
    * @param elt Construct to test.
    */
   static isCustomResource(elt: Construct): boolean {
-    return CfnResource.isCfnResource(elt)
-      && CfnElement.isCfnElement(elt) // isCfnResource isn't good enough by itself.
+    return CfnElement.isCfnElement(elt) // isCfnResource isn't good enough by itself.
+      && CfnResource.isCfnResource(elt)
       && (elt.cfnResourceType.startsWith('Custom::') ||
-    elt.cfnResourceType == 'AWS::CloudFormation::CustomResource');
+          elt.cfnResourceType == 'AWS::CloudFormation::CustomResource');
   }
-
-  protected treeSearch = ConstructTreeSearch.for(CustomResourceUtilities.isCustomResource);
 
   /**
    * Returns a list of all L1 custom resources under the scope.
    * @param scope Scope for the search.
+   * @param resourceType Must be of the form 'Custom::XXX' or 'AWS::CloudFormation::CustomResource'.  Optional.
+   * @param predicate Optional predicate.
    */
-  customResources(scope: Construct) {
-    return this.treeSearch.searchDown(scope, Stack.isStack) as CfnResource[];
+  customResources(scope: Construct, resourceType?: string, predicate?: CfnResourcePredicate) {
+    return ConstructTreeSearch.for(
+      x => CustomResourceUtilities.isCustomResource(x) && CfnResource.isCfnResource(x) &&
+      (resourceType == undefined || resourceType == (x).cfnResourceType) &&
+      (predicate == undefined || predicate(x)),
+    ).searchDown(scope, Stack.isStack) as CfnResource[];
   }
 
   /**
    * Returns the CfnResource that produces the custom resource.  This function throws
    * if there are none (or more than one).
-   * @param target
+   * @param scope Scope for the search.
+   * @param resourceType Must be of the form 'Custom::XXX' or 'AWS::CloudFormation::CustomResource'.  Optional.
+   * @param predicate Optional predicate.
    */
-  findCustomResource(target: Construct) {
-    let elements = this.treeSearch.searchDown(target);
+  findCustomResource(scope: Construct, resourceType?: string, predicate?: CfnResourcePredicate) {
+    let elements = this.customResources(scope, resourceType, predicate);
     if (elements.length != 1) {
       throw new Error(`Construct ${
-        target.node.path
+        scope.node.path
       } is not a known custom resource type.  Found ${elements.length} CustomResources, expected one (1).`);
     }
 
